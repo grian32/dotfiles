d1381fd45e80b5949fe5760030cc27c4df973ee7
grian32 <grianguy32@gmail.com>
2025-06-26 19:16:05 +0300

    feat: manjaro gnome central script adds

---
+++ manjaro-gnome.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo ./manjaro-gnome/setupyay.sh
+./manjaro-gnome/setupghcli.sh
+./manjaro-gnome/setupbrave.sh
+./manjaro-gnome/yayprograms.sh
+./manjaro-gnome/pacmanprograms.sh
+./manjaro-gnome/setupflameshot.sh
+echo "Finished installation! Please be on the lookout for any possible errors in the logs! Remember to login to your various account and sync Brave Browser"x
--- manjaro-gnome/manjaro-gnome.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo ./manjaro-gnome/setupyay.sh
-./manjaro-gnome/setupghcli.sh
-./manjaro-gnome/setupbrave.sh
-./manjaro-gnome/yayprograms.sh
-./manjaro-gnome/pacmanprograms.sh
