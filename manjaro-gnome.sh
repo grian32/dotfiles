3ce02df4e3d607e945e2a8eb566d71db743f7026
grian32 <grianguy32@gmail.com>
2025-07-17 18:21:38 +0300

    feat: fetch zed

--- manjaro-gnome.sh
+++ manjaro-gnome.sh
@@ -6,4 +6,5 @@ sudo ./manjaro-gnome/setupyay.sh
 sudo ./manjaro-gnome/pacmanprograms.sh
 ./manjaro-gnome/setupflameshot.sh
 ./manjaro-gnome/setuptoolbox.sh
+./manjaro-gnome/setupzed.sh
 echo "Finished installation! Please be on the lookout for any possible errors in the logs! Remember to login to your various account and sync Brave Browser"x
---
+++ manjaro-gnome/setupzed.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Starting Zed Install"
+curl -f https://zed.dev/install.sh | sh
+echo "Finishing Zed Install"
